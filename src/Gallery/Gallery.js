7ffb2b4c9e3f43f93396e6b1d9597bf20a55c8c1
Alexey Komov <alexeykcontact@gmail.com>
2020-06-09 19:27:52 +0300

    add: list layout for gallery

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -3,25 +3,58 @@ import { Card, Typography, CardContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import CardMedia from '@material-ui/core/CardMedia';
 import { differenceWith, unionWith } from 'lodash';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
     maxWidth: 275,
   },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '0 auto',
-    maxWidth: '825px',
-    width: '100%',
+  header: {
+    color: 'rgb(141,175,192)',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    paddingTop: '16px',
+    paddingLeft: '16px',
+    paddingRight: '16px',
   },
-  containerNarrow: {
+  mainContainer: {
+    flexDirection: 'row',
+    display: 'flex',
+    height: '100%',
+
+
+    position: 'absolute',
+  },
+  readingContainer: {
+    backgroundColor: 'rgb(233,239,246)',
+    flex: 1,
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'auto',
+    minHeight: 0,
+    height: '100%',
+  },
+  listContainer: {
+    maxWidth: '320px',
+    backgroundColor: '#fff',
+  },
+  listItemSelected: {
+    backgroundColor: 'rgb(233,239,246)',
+  },
+  itemText: {
+    color: 'rgb(121,120,121)',
+  },
+  pageView: {
+    width: '80%',
+    cursor: 'grab',
   },
   title: {
     fontSize: 14,
@@ -50,8 +83,16 @@ const shouldRenderNewDocuments = (oldDocuments, newDocuments) => {
 
 function Gallery({ setDocuments, documents: aDocuments }) {
   const classes = useStyles();
-  let cards = [];
   const history = useHistory();
+  const { id } = useParams();
+  console.log('id: ', id);
+  let items = [
+    { name: "Driver's License", selected: false },
+    { name: 'Your Photo', selected: false },
+    { name: 'Insurance Card', selected: true },
+    { name: 'W2', selected: false },
+    { name: 'Pay Stub', selected: false },
+  ].map((item, index) => ({ ...item, selected: id === String(index) }));
 
   useEffect(() => {
     fetch('./doc')
@@ -64,39 +105,38 @@ function Gallery({ setDocuments, documents: aDocuments }) {
       .catch((e) => console.log(`Error when fetching documents: ${e}`));
   });
   const makeOnClick = (cardId) => () => {
-    history.push(`/item/${cardId}`);
+    history.push(`/gallery/${cardId}`);
   };
-  const screenIsNarrow = useMediaQuery('(max-width:550px)');
-  console.log('screenIsNarrow: ', screenIsNarrow);
 
-  cards = aDocuments.map((document, index) => (
-    <Card className={classes.root} key={document.url} square={true}>
-      <CardMedia
-        className={classes.media}
-        image={document.url}
-        title={document.name}
-      />
-      <CardContent>
-        <Typography variant="body2" component="p">
-          {document.name}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={makeOnClick(document.name)}>
-          Open
-        </Button>
-      </CardActions>
-    </Card>
+  const itemsElements = items.map((item, index) => (
+    <ListItem
+      button
+      onClick={makeOnClick(index)}
+      className={item.selected ? classes.listItemSelected : {}}
+      key={item.name}
+    >
+      <ListItemText primary={item.name} className={classes.itemText} />
+    </ListItem>
   ));
 
   return (
-    <div
-      className={[
-        ...[classes.container],
-        ...(screenIsNarrow ? [classes.containerNarrow] : []),
-      ]}
-    >
-      {cards}
+    <div className={classes.mainContainer}>
+      <div className={classes.listContainer}>
+        <Typography variant="body1" component="h3" className={classes.header}>
+          Your Uploaded Documents (5)
+        </Typography>
+        <List component="nav" aria-label="List of documents">
+          {itemsElements}
+        </List>
+      </div>
+      <div className={classes.readingContainer}>
+        <img
+          width={300}
+          src={`../screen${id}.png`}
+          alt={'Стив Макконнелл - Совершенный код, 2-е издание - 2010.png'}
+          className={classes.pageView}
+        />
+      </div>
     </div>
   );
 }
